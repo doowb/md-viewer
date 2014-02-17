80818a572702992ea110a8f59207f09f58206d1d
Brian Woodward <brian.woodward@gmail.com>
2014-02-17 12:59:50 -0500

    converting tabs to spaces because the default from the express generator was tabs

--- routes/markdown.js
+++ routes/markdown.js
@@ -9,29 +9,29 @@ var file = require('fs-utils');
 var _ = require('lodash');
 
 exports.markdown = function (base) {
-	
-	var root = dirname(base);
-	var layout = file.readFileSync(__dirname + '/../views/layout.hbs');
+  
+  var root = dirname(base);
+  var layout = file.readFileSync(__dirname + '/../views/layout.hbs');
 
-	return function(req, res, next) {
-		
-		var path = req.path;
-		if (path === '/') {
-			path = '/index.md';
-		}
+  return function(req, res, next) {
+    
+    var path = req.path;
+    if (path === '/') {
+      path = '/index.md';
+    }
 
-		if (path.indexOf('.md') === -1) {
-			return res.send(file.readFileSync(__dirname + '/../public' + path));
-		}
+    if (path.indexOf('.md') === -1) {
+      return res.send(file.readFileSync(__dirname + '/../public' + path));
+    }
 
-		var body = file.readFileSync(root(path));
-		assemble(layout.replace(/{{>body}}/, body)).build(function (err, results) {
-			if (err) {
-				return res.error(err);
-			}
+    var body = file.readFileSync(root(path));
+    assemble(layout.replace(/{{>body}}/, body)).build(function (err, results) {
+      if (err) {
+        return res.error(err);
+      }
 
-			var content = results.components[_.keys(results.components)[0]].content;
-			res.send(content);
-		});
-	};
+      var content = results.components[_.keys(results.components)[0]].content;
+      res.send(content);
+    });
+  };
 };
